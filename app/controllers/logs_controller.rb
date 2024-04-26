0a7832bab5c26f3bdb2190dc056722af18c3d7d5
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2024-04-26 11:01:50 +0900

    lazy loading

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,5 +1,7 @@
 class LogsController < ApplicationController
   def index
-    @logs = Log.order(created_at: :asc)
+    @lower_id = params[:lower_id].present? ? params[:lower_id].to_i : nil
+    @logs = Log.fetch(lower_id: @lower_id).order(created_at: :asc)
+    @next_lower_id = @logs.first&.id
   end
 end
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,2 +1,10 @@
 class Log < ApplicationRecord
+  MAX_FETCH_SIZE = 40
+
+  class << self
+    def fetch(lower_id: nil)
+      lower_id ||= last.id
+      where(id: ((lower_id - MAX_FETCH_SIZE)...lower_id).to_a)
+    end
+  end
 end
